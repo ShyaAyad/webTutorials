a0e749f35e2b1b1853a3bc90ec823e2759d716fa
rasathelamedude <rasyar.safen19@gmail.com>
2025-07-02 17:08:40 +0300

    Defined api endpoints for tutorials :3

---
+++ backend/routes/tutorial.routes.js
@@ -0,0 +1,19 @@
+// Defining endpoints for actions related to tutorials;
+import { Router } from "express";
+
+const tutorialRouter = Router();
+
+tutorialRouter.get("/", getAllTutorials); // get all tutorials;
+tutorialRouter.get("/categories/frontend", getFrontendTutorials); // get frontend tutorials;
+tutorialRouter.get("/categories/backend", getBackendTutorials); // get backend tutorials;
+tutorialRouter.get("/categories/apis", getAPITutorials); // get API tutorials;
+tutorialRouter.get("/categories/uiux", getUIUXTutorials); // get UIUX tutorials;
+tutorialRouter.get("/:tutorialId", getTutorialById); // get a specific tutorial by id;
+
+tutorialRouter.post("/", createTutorial); // create a tutorial;
+
+tutorialRouter.patch("/:tutorialId", updateTutorial); // update a tutorial;
+
+tutorialRouter.delete("/:tutorialId", deleteTutorial); // delete a tutorial;
+
+export default tutorialRouter;
