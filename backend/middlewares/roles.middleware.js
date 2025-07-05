ed2662c96e61999f93db7653c6b0bb10799cca05
rasathelamedude <rasyar.safen19@gmail.com>
2025-07-05 16:40:56 +0300

    this was all buggy :/. I just realized you cannot use a function that returns a middleware as a middleware. The function itself should be the middleware.

--- backend/middlewares/roles.middleware.js
+++ backend/middlewares/roles.middleware.js
@@ -1,12 +1,10 @@
-export const checkRole = async () => {
-  return (request, response, next) => {
-    if (request.user || request.user.role !== "Admin") {
-      response.status(403).json({
-        success: false,
-        message: "Access denied. Not enough permission.",
-      });
-    }
+export const checkRole = async (request, response, next) => {
+  if (!request.user || request.user.role !== "Admin") {
+    response.status(403).json({
+      success: false,
+      message: "Access denied. Not enough permission.",
+    });
+  }
 
-    next();
-  };
+  next();
 };
